73441e106ae6f98b114e040d408f166ec4218ba3
Charaka Ilangarathne <it21319242@my.sliit.lk>
2023-05-15 21:36:24 +0530

    test

--- app/src/main/java/com/example/familyshare2/FamilyShareHome.kt
+++ app/src/main/java/com/example/familyshare2/FamilyShareHome.kt
@@ -3,6 +3,7 @@ package com.example.familyshare2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.familyshare2.databinding.ActivityFamilyShareHomeBinding
 
 class FamilyShareHome : AppCompatActivity() {
@@ -14,10 +15,14 @@ class FamilyShareHome : AppCompatActivity() {
         binding = ActivityFamilyShareHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.addMenberCard.setOnClickListener {
+        binding.addMemberCard.setOnClickListener {
             val intent = Intent(this, FamilySharingAddMember::class.java)
             startActivity(intent)
 
+        }
+        fun redirectAddMember(view: View){
+            val intent = Intent(this, FamilySharingAddMember::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/familyshare2/models/MembersData.kt
+++ app/src/main/java/com/example/familyshare2/models/MembersData.kt
@@ -3,13 +3,11 @@ package com.example.familyshare2.models
 class MembersData {
     var fid: String? = null
     var email: String? = null
+    var status: String="Pending"
 
     constructor(fid: String?, email: String?){
         this.fid = fid
         this.email = email
     }
 
-    constructor(){
-
-    }
 }
